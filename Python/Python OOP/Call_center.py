55e41836bb4237dbb1d5f1d44e7d591925c86f49
aigooitzj0e <aigooitzj0e@gmail.com>
2017-08-05 13:41:57 -0700

    updated python assignments

--- Python/Python OOP/Call_center.py
+++ Python/Python OOP/Call_center.py
@@ -17,11 +17,22 @@ class Call(object):
 bob = Call(01, "bob", 2, 6, "ok")
 bob.display()
 
-class CallCenter(call):
-	def __init__(self, name, phone):
+class CallCenter(Call):
+	def __init__(self, Call):
 		super(CallCenter, self).__init__(name, phone)
 		self.call = []
 		self.que = len(self.call)
 
 	def add(self):
-		self.call.append()
+		self.call.append(Call)
+
+
+	def remove(self):
+		self.call.pop(0)
+
+	def info(self):
+		for caller in self.call:
+			for i in caller:
+				print caller[1]
+				print caller[2]
+
