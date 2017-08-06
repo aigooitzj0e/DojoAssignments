76d1522a0c00933a7c7708cf498df8703bbffdfe
Joe Yu <joeyu@Joes-MacBook-Pro.local>
2017-08-05 18:19:35 -0700

    added python oop assignment

--- Python/Python OOP/Call_center.py
+++ Python/Python OOP/Call_center.py
@@ -1,38 +1,30 @@
-class Call(object):
-	def __init__(self, idd, name, phone, time, reason):
-		self.id = idd
-		self.name = name
-		self.phone = phone
-		self.time = time
-		self.reason = reason
 
-	def display(self):
-		print self.id
-		print self.name
-		print self.phone
-		print self.time
-		print self.reason
+class Call():
+    def __init__(self,ID,caller_name,time,reason):
+        self.ID = ID
+        self.caller = caller_name
+        self.time = time
+        self.reason = reason
+    def display(self):
+        print "ID: " + self.ID
+        print "Caller Name:" + self.caller
+        print "Time of call: " + self.time
+        print "Reason for call: " + self.reason
 
+Call("01","Christian","9:00 AM","Needs backrub").display()
 
-bob = Call(01, "bob", 2, 6, "ok")
-bob.display()
+class callCenter():
+    def __init__(self,call):
+        self.call = call
+        self.queue = len(self.call)
+    def add(self):
+        self.call.append(self.call[len(self.call)-1]+1)
+        return self
+    def remove(self):
+        self.call.remove(self.call[0])
+        return self
+    def info(self):
+        print str(self.queue) + " in Queue"
+        return self
 
-class CallCenter(Call):
-	def __init__(self, Call):
-		super(CallCenter, self).__init__(name, phone)
-		self.call = []
-		self.que = len(self.call)
-
-	def add(self):
-		self.call.append(Call)
-
-
-	def remove(self):
-		self.call.pop(0)
-
-	def info(self):
-		for caller in self.call:
-			for i in caller:
-				print caller[1]
-				print caller[2]
-
+callCenter([1348534,3455222,55488483,932582375,72352525]).remove().info()
--- Python/Python OOP/hospital.py
+++ Python/Python OOP/hospital.py
@@ -1,3 +1,11 @@
+class patient():
+	def __init__(self, idd, name, allergies, bed):
+		self.idd = idd
+		self.name = name
+		self.allergies = allergies
+		self.bed_num = bed
+
+
 class hospital(object):
 	def __init__(self,hospital_name, capactiy):
 		self.patient_list = []
@@ -7,10 +15,16 @@ class hospital(object):
 		self.count = 0
 
 	def admit(self):
-		self.patient_list.append(patient_name) #append patient name to patient list
+		self.patient_list.append(self.name) #append patient name to patient list
 		self.count += 1
 		print bed_num
 		
 	def discharge(self):
-		self.search = patient_list.index(patient_name) #searches to see what index the patient is at in list. returns index #
+		self.search = patient_list.index(self.name) #searches to see what index the patient is at in list. returns index #
 		self.patient_list.pop(self.search) #removes patient at designated index
+
+
+bob = patient("01", "Bob", "Peanuts", "01")
+
+hospital1 = hospital("Kahwea", 100)
+hospital1.admit()
