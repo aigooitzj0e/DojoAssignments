1a309ee1148db8d45bb7fcc51d7e9471eaa0d537
Joe Yu <joeyu@Joes-MacBook-Pro.local>
2017-08-07 21:33:12 -0700

    hospital updated

--- Python/Python OOP/hospital.py
+++ Python/Python OOP/hospital.py
@@ -1,41 +1,56 @@
-class patient(object):
-	def __init__(self, idd, name, allergies, bed):
-		self.idd = idd
+import random
+class Patient(object):
+	def __init__(self,unID,name, allergies):
+		self.unID = unID
 		self.name = name
 		self.allergies = allergies
-		self.bed_num = bed
+		self.bedNumber = 0
+
+	def display(self):
+		print 'ID:',self.unID
+		print 'Name:',self.name
+		print 'Allergies:',self.allergies
+		print 'Bed number:',self.bedNumber
+Joe = Patient(1,'Joe','intellegence')
+Joe.display()
+print "____________________"
+print "____________________"
 
 
-class hospital(patient):
-	def __init__(self, idd, name, allergies, bed):
-		super(hospital, self).__init__(idd, name, allergies, bed)
-		self.patient_list = []
-		self.hospital_name = "Kaweah"
-		self.capacity = 100
+
+
+class Hospital(object):
+	def __init__(self,name,capacity):
+		self.patients = []
+		self.name = name
+		self.capacity = capacity
 		self.count = 0
-
-	def admit(self):
-		self.patient_list.append(self.name) #append patient name to patient list
+	def admit(self, patient):
+		self.patients.append(patient)
 		self.count += 1
-		print "Patient added.. Total patients is:", self.count
-		print self.patient_list
+		if self.count >= self.capacity:
+			print 'You are not welcome'
+		return self
+	def discharge(self,patient):
+		self.patients.remove(patient)
+		return self
+	def info(self):
+		for i in self.patients:
+			print 'ID:',i.unID
+			print 'Name:',i.name
+			print 'Allergies:',i.allergies
+			for i in range(self.capacity):
+				bedNumber = random.randint(1,self.capacity)
+			print "bed number:",bedNumber
+			print " "
 		return self
 
-	def discharge(self):
-		self.search = self.patient_list.index(self.name) #searches to see what index the patient is at in list. returns index #
-		self.patient_list.pop(self.search) #removes patient at designated index
-		self.count -= 1
-		print "Patient released.. Total patients is:", self.count
-		return self
-
-# bob = patient("01", "Bob", "Peanuts", "01")
-
-pat1 = ('01', 'bob', 'peanuts', "001")
-pat2 = ('02', 'larry', 'milk', "002")
-pat3 = ('03', 'tom', 'orange', "003")
-pat4 = ('04', 'susan', 'grass', "004")
-
-hos1 = hospital(pat1, pat2, pat3, pat4)
+		
+patient1 = Patient(2,"Cristian","losers")
+patient2 = Patient(77,"Joe","intaligens")
+patient3 = Patient(3,'Will', "air")
+hospital = Hospital('Moncler',3)
 
 
-hos1.admit().admit().discharge()
+
+hospital.admit(patient1).admit(patient2).admit(patient3).discharge(patient2).info()
